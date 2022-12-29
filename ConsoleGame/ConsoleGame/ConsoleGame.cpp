efcb319769fa708734409c3bb924aa75aead38f1
Serranoivi <74309140+Serranoivi@users.noreply.github.com>
2022-12-29 19:05:01 +0100

    cambio del tamaño de mapa a 20 para generar conflicto

--- ConsoleGame/ConsoleGame/ConsoleGame.cpp
+++ ConsoleGame/ConsoleGame/ConsoleGame.cpp
@@ -18,7 +18,7 @@ void setConsoleCursorVisible(bool showFlag)
 int main()
 {
   setConsoleCursorVisible(false);
-  const int size = 15;
+  const int size = 20;
   char* game = new char[size];
   game[size - 1] = '\0';
 
